b62cf81d3a567c1e15da83b4f4db293c00ff2972
Theo Vora <theo.vora@gmail.com>
2023-06-25 11:56:51 -0400

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Theo Vora on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
